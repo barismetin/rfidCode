79882126f035099e38a74336146c71aaa6655c97
barisMetin <me@barismetin.com>
2016-05-07 12:48:57 +0300

    rf id cart

--- rf_d/rf_d.ino
+++ rf_d/rf_d.ino
@@ -1,98 +1,126 @@
-#include <SPI.h>
 #include <MFRC522.h>
-#include <EEPROM.h>
- 
-#define RST_PIN 9
-#define SS_PIN 10
-#define ledPin 7
- 
-MFRC522 mfrc522(SS_PIN, RST_PIN);
- 
-String lastRfid = "";
-String kart1 = "";
-String kart2 = "";
- 
-MFRC522::MIFARE_Key key;
- 
-void setup()
-{
-  Serial.begin(9600);
+//Get library from https://github.com/ljos/MFRC522
+//Sketch: gudjonholm@gmail.com
+#include <SPI.h>
+
+/*
+Pins  SPI    UNO  Mega2560  Leonardo
+1 (NSS) SAD (SS)   10     53        10
+2       SCK        13     52        SCK1
+3       MOSI       11     51        MOSI1
+4       MISO       12     50        MISO1
+5       IRQ        *      *         *
+6       GND       GND     GND       GND
+7       RST        5      ?         Reset
+8      +3.3V (VCC) 3V3     3V3       3.3V
+* Not needed
+1 on ICPS header
+*/
+#define SAD 10
+#define RST 5
+MFRC522 nfc(SAD, RST);
+
+
+#define ledPinOpen    2
+#define ledPinClosed  3
+
+
+void setup() {
+  pinMode(ledPinOpen  , OUTPUT);   
+  pinMode(ledPinClosed, OUTPUT);   
   SPI.begin();
-  mfrc522.PCD_Init();
-  pinMode(ledPin, OUTPUT);
-  Serial.println("RFID KART OKUMA UYGULAMASI");
-  Serial.println("--------------------------");
-  Serial.println();
-  //EEPROM'dan kart bilgisini oku
-  readEEPROM();
+  Serial.begin(115200);
+  Serial.println("Looking for MFRC522.");
+  nfc.begin();
+  byte version = nfc.getFirmwareVersion();
+  if (! version) {
+    Serial.print("Didn't find MFRC522 board.");
+    while(1); //halt
+  }
+
+  Serial.print("Found chip MFRC522 ");
+  Serial.print("Firmware ver. 0x");
+  Serial.print(version, HEX);
+  Serial.println(".");
 }
- 
-void loop()
+
+#define AUTHORIZED_COUNT 1 /*If you want more Authorized of cards set the count here, and then add the serials below*/
+byte Authorized[AUTHORIZED_COUNT][6] = {
+
+                            {0x45, 0x14, 0x39, 0x2E, 0xFF, 0xFF, }
+                           /*,{0x10, 0x14, 0x39, 0x2E, 0xFF, 0xFF, }*/  /*f. example how to add more authorized cards*/
+                            
+                          };
+                          
+void printSerial(byte *serial);
+boolean isSame(byte *key, byte *serial);
+boolean isAuthorized(byte *serial);
+
+
+
+void loop() {
+  byte status;
+  byte data[MAX_LEN];
+  byte serial[5];
+  boolean Opening = false;
+  digitalWrite(ledPinOpen, Opening);
+  digitalWrite(ledPinClosed, !Opening);
+  status = nfc.requestTag(MF1_REQIDL, data);
+
+  if (status == MI_OK) {
+    status = nfc.antiCollision(data);
+    memcpy(serial, data, 5);
+    
+    if(isAuthorized(serial))
+    { 
+      Serial.println("Authenticated");
+      Opening = true;
+    }
+    else
+    { 
+      printSerial(serial);
+      Serial.println("is NOT authenticated");
+      Opening = false;
+    }
+    
+    nfc.haltTag();
+    digitalWrite(ledPinOpen, Opening);
+    digitalWrite(ledPinClosed, !Opening);
+    delay(2000);
+  }//if (status == MI_OK)
+
+  delay(500);
+
+}//void loop()
+
+boolean isSame(byte *key, byte *serial)
 {
-  //yeni kart okununmadıkça devam etme
-  if ( ! mfrc522.PICC_IsNewCardPresent())
-  {
-    return;
-  }
-  if ( ! mfrc522.PICC_ReadCardSerial())
-  {
-    return;
-  }
-  //kartın UID'sini oku, rfid isimli string'e kaydet
-  String rfid = "";
-  for (byte i = 0; i < mfrc522.uid.size; i++)
-  {
-    rfid += mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ";
-    rfid += String(mfrc522.uid.uidByte[i], HEX);
-  }
-  //string'in boyutunu ayarla ve tamamını büyük harfe çevir
-  rfid.trim();
-  rfid.toUpperCase();
-  
-  if (rfid == lastRfid)
-    return;
-  lastRfid = rfid;
- 
-  Serial.print("Kart 1: ");
-  Serial.println(kart1);
-  Serial.print("Kart 2: ");
-  Serial.println(kart2);
-  Serial.print("Okunan: ");
-  Serial.println(rfid);
-  Serial.println();
-  //1 nolu kart okunduysa LED'i yak, 2 nolu kart okunduysa LED'i söndür
-  if (rfid == kart1)
-  {
-    digitalWrite(ledPin, HIGH);
-    Serial.println("LED yandi.");
-  }
-  if (rfid == kart2)
-  {
-    digitalWrite(ledPin, LOW);
-    Serial.println("LED sondu.");
-  }
-  Serial.println();
-  delay(200);
- 
+    for (int i = 0; i < 4; i++) {
+      if (key[i] != serial[i])
+      { 
+        return false; 
+      }
+    }
+    
+    return true;
+
 }
- 
-void readEEPROM()
+
+boolean isAuthorized(byte *serial)
 {
-  //EEPROM'dan ilk kartın UID'sini oku (ilk 4 byte)
-  for (int i = 0 ; i < 4 ; i++)
-  {
-    kart1 += EEPROM.read(i) < 0x10 ? " 0" : " ";
-    kart1 += String(EEPROM.read(i), HEX);
-  }
-  //EEPROM'dan ikinci kartın UID'sini oku
-  for (int i = 4 ; i < 8 ; i++)
-  {
-    kart2 += EEPROM.read(i) < 0x10 ? " 0" : " ";
-    kart2 += String(EEPROM.read(i), HEX);
-  }
-  //Okunan stringleri düzenle
-  kart1.trim();
-  kart1.toUpperCase();
-  kart2.trim();
-  kart2.toUpperCase();
+    for(int i = 0; i<AUTHORIZED_COUNT; i++)
+    {
+      if(isSame(serial, Authorized[i]))
+        return true;
+    }
+   return false;
+}
+
+void printSerial(byte *serial)
+{
+        Serial.print("Serial:");
+    for (int i = 0; i < 4; i++) {
+      Serial.print(serial[i], HEX);
+      Serial.print(" ");
+    }
 }
